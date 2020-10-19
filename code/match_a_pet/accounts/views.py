9cf53ad00331c49d43e6ccdae5a129f37fa69b72
gururaj3 <guruz.shinde@gmail.com>
2020-10-18 23:10:47 -0400

    updated views.py

--- code/match_a_pet/accounts/views.py
+++ code/match_a_pet/accounts/views.py
@@ -5,23 +5,26 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import ShelterRegistrationForm
+from .forms import ShelterRegistrationForm, ShelterUpdateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
 
+global form
 
 def home(request):
     return render(request, 'accounts/home.html')
 
 def registerShelter(request):
     if request.method == 'POST':
+         
         form = ShelterRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('shelter_name')
-            messages.success(request, f'Account created for {username}!')
-            return render(request, 'accounts/login.html', {'form':form})
+            email = form.cleaned_data.get('email')
+            messages.success(request, f'Account succesfully created for {email} !')
+            return redirect('/login/shelter')
     else:
         form = ShelterRegistrationForm()
     return render(request, 'accounts/register.html', {'form':form})
@@ -31,5 +34,20 @@ def registerUser(request):
 
 def loginShelter(request):
     return render(request, 'accounts/login.html')
+
+@login_required
+def shelterProfile(request):
+    if request.method == 'POST':
+        shelterUpdateForm = ShelterUpdateForm(request.POST, instance=request.user)
+        if shelterUpdateForm.is_valid():
+            shelterUpdateForm.save()
+            messages.success(request, f'Account succesfully updated!')
+            return redirect('/profile/shelter')
+    else:
+        shelterUpdateForm = ShelterUpdateForm(instance=request.user)
+
+    context = {'shelterUpdateForm':shelterUpdateForm}
+
+    return render(request, 'accounts/shelterProfile.html', context)
 
 
