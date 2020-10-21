eb520fe706a3d31f75cac4e27572cdb0910fd053
gururaj3 <guruz.shinde@gmail.com>
2020-10-21 12:15:54 -0400

    fixing issues

--- code/match_a_pet/accounts/forms.py
+++ code/match_a_pet/accounts/forms.py
@@ -2,6 +2,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import ShelterRegisterData, Pet
+from django.forms import ModelForm
+from django.db import models
+
 
 class ShelterRegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -11,13 +15,45 @@ class ShelterRegistrationForm(UserCreationForm):
         self.fields['first_name'].label = 'Shelter Staff First Name'
         self.fields['last_name'].label = 'Shelter Staff Last Name'
 
-    l_choices = (('1','New York'), ('2','California'))
+    l_choices = (('New York','New York'), ('California','California'))
+    ny_choices = (('Manhattan','Manhattan'), ('Brooklyn','Brooklyn'), ('Queens','Queens'), ('Staten Island','Staten Island'), ('Bronx','Bronx'))
     email = forms.EmailField(required = True)
     first_name = forms.CharField(required = True)
     last_name = forms.CharField(required = True)
+    shelter_city = forms.ChoiceField(choices = ny_choices)
     shelter_state = forms.ChoiceField(choices = l_choices)
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'shelter_state', 'password1', 'password2',]
+        model = ShelterRegisterData
+        fields = ['username', 'email', 'first_name', 'last_name', 'shelter_city', 'shelter_state', 'password1', 'password2',]
         help_texts = {'username': ('Shelter name can contain Letters, digits and @/./+/-/_ only.')}
+
+class ShelterUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].label = 'Shelter Email'
+        self.fields['username'].label = 'Shelter Name'
+        self.fields['first_name'].label = 'Shelter Staff First Name'
+        self.fields['last_name'].label = 'Shelter Staff Last Name'
+
+    l_choices = (('New York','New York'), ('California','California'))
+    ny_choices = (('Manhattan','Manhattan'), ('Brooklyn','Brooklyn'), ('Queens','Queens'), ('Staten Island','Staten Island'), ('Bronx','Bronx'))
+    email = forms.EmailField(required = True)
+    first_name = forms.CharField(required = True)
+    last_name = forms.CharField(required = True)
+    shelter_city = forms.ChoiceField(choices = ny_choices)
+    shelter_state = forms.ChoiceField(choices = l_choices)
+
+    class Meta:
+        model = ShelterRegisterData
+        fields = ['username', 'email', 'first_name', 'last_name', 'shelter_city', 'shelter_state',]
+        help_texts = {'username': ('Shelter name can contain Letters, digits and @/./+/-/_ only.')}
+
+
+class PetForm(forms.ModelForm):
+
+    # shelter_id = forms.CharField(disabled = True)
+    # email = forms.CharField(disabled = True)
+    class Meta:
+        model = Pet
+        fields = [ 'pet_name', 'pet_breed', 'pet_age', 'pet_color', 'pet_gender', 'date_entered']
--- code/match_a_pet/accounts/views.py
+++ code/match_a_pet/accounts/views.py
@@ -5,23 +5,26 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import ShelterRegistrationForm
+from .forms import ShelterRegistrationForm, ShelterUpdateForm, PetForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
 
+global form
 
 def home(request):
     return render(request, 'accounts/home.html')
 
 def registerShelter(request):
     if request.method == 'POST':
+         
         form = ShelterRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('shelter_name')
-            messages.success(request, f'Account created for {username}!')
-            return render(request, 'accounts/login.html', {'form':form})
+            email = form.cleaned_data.get('email')
+            messages.success(request, f'Account succesfully created for {email} !')
+            return redirect('/login/shelter')
     else:
         form = ShelterRegistrationForm()
     return render(request, 'accounts/register.html', {'form':form})
@@ -31,5 +34,39 @@ def registerUser(request):
 
 def loginShelter(request):
     return render(request, 'accounts/login.html')
+
+def petsRegister(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit = False)
+            instance.email = request.user
+            instance.save()
+            form.save()
+            pet = form.cleaned_data.get('pet_name')
+            messages.success(request, f'Pet profile created for {pet}!')
+            return render(request, 'accounts/pets.html', {'form': form})
+
+    else:
+        form = PetForm()
+    return render(request, 'accounts/pets.html', {'form': form})
+
+@login_required
+def shelterProfile(request):
+    if request.method == 'POST':
+        shelterUpdateForm = ShelterUpdateForm(request.POST, instance=request.user)
+        if shelterUpdateForm.is_valid():
+            shelterUpdateForm.save()
+            messages.success(request, f'Account succesfully updated!')
+            return redirect('/profile/shelter')
+    else:
+        shelterUpdateForm = ShelterUpdateForm(instance=request.user)
+
+    context = {'shelterUpdateForm':shelterUpdateForm}
+
+    return render(request, 'accounts/shelterProfile.html', context)
+
+
+
 
 
