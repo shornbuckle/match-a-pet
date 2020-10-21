c46817875fe301545f78f1762dc72b45c721f1d5
gururaj3 <guruz.shinde@gmail.com>
2020-10-20 22:48:29 -0400

    updated views.py file

--- code/match_a_pet/accounts/views.py
+++ code/match_a_pet/accounts/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import ShelterRegistrationForm, ShelterUpdateForm
+from .forms import ShelterRegistrationForm, ShelterUpdateForm, PetForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -35,6 +35,22 @@ def registerUser(request):
 def loginShelter(request):
     return render(request, 'accounts/login.html')
 
+def petsRegister(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit = False)
+            instance.email = request.user
+            instance.save()
+            form.save()
+            pet = form.cleaned_data.get('pet_name')
+            messages.success(request, f'Pet profile created for {pet}!')
+            return render(request, 'accounts/pets.html', {'form': form})
+
+    else:
+        form = PetForm()
+    return render(request, 'accounts/pets.html', {'form': form})
+
 @login_required
 def shelterProfile(request):
     if request.method == 'POST':
@@ -49,5 +65,8 @@ def shelterProfile(request):
     context = {'shelterUpdateForm':shelterUpdateForm}
 
     return render(request, 'accounts/shelterProfile.html', context)
+
+
+
 
 
