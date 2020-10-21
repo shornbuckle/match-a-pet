3f4e30bf8e5c5e12e5d2cbc4ae54369125239cdb
gururaj3 <guruz.shinde@gmail.com>
2020-10-20 22:45:35 -0400

    updated admin page to show pet data

--- code/match_a_pet/accounts/admin.py
+++ code/match_a_pet/accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import ShelterRegisterData
+from .models import ShelterRegisterData, Pet
 
 # Register your models here.
+
+admin.site.site_header = 'Math-A-Pet Admin Page'
 
 class RegisterShelterAdmin(UserAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name', 'shelter_city', 'shelter_state', 'last_login', 'is_active', 'is_staff', 'is_admin', 'is_superuser')
@@ -14,3 +16,13 @@ class RegisterShelterAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(ShelterRegisterData, RegisterShelterAdmin)
+
+class PetRegisterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'pet_name', 'pet_breed', 'pet_age', 'pet_color', 'pet_gender', 'date_entered')
+    search_fields = ('pet_name', 'pet_breed',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Pet, PetRegisterAdmin)
