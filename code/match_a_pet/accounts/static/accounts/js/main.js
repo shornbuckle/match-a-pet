41b308ae7cf1b0c7e79a3c6e52682ea8f4be0f8f
shrutiagarwal28 <agarwalshruti2801@gmail.com>
2020-10-27 09:27:38 -0400

    Pop-up added

--- code/match_a_pet/accounts/static/accounts/js/main.js
+++ code/match_a_pet/accounts/static/accounts/js/main.js
@@ -251,3 +251,9 @@
 
 })(jQuery);
 
+function openLoginForm(){
+	document.body.classList.add("showLoginForm");
+  }
+  function closeLoginForm(){
+	document.body.classList.remove("showLoginForm");
+  }
