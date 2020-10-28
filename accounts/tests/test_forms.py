f733b5c2cc0bf9f223a71331661f7d59057d245a
gururaj3 <guruz.shinde@gmail.com>
2020-10-28 09:28:31 -0400

    adding more tests

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,7 +1,5 @@
 from django.test import SimpleTestCase, TestCase
 from accounts.forms import ShelterRegistrationForm, ShelterUpdateForm, PetForm
-from django.urls import reverse
-from accounts.models import ShelterRegisterData
 
 
 class DefaultTestFroms(TestCase):
@@ -31,6 +29,97 @@ class DefaultTestFroms(TestCase):
         form = ShelterRegistrationForm(
             data={
                 "username": "peter7",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 7)
+
+    def test_ShelterRegistrationForm_invalid_data1(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+                "shelter_city": "Manhattan",
+                "shelter_state": "New York",
+                "password1": "test123abc",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+
+    def test_ShelterRegistrationForm_invalid_data2(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+                "shelter_city": "Manhattan",
+                "shelter_state": "New York",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 2)
+
+    def test_ShelterRegistrationForm_invalid_data3(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+                "shelter_city": "Manhattan",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 3)
+
+    def test_ShelterRegistrationForm_invalid_data4(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 4)
+
+    def test_ShelterRegistrationForm_invalid_data5(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 5)
+
+    def test_ShelterRegistrationForm_invalid_data6(self):
+        form = ShelterRegistrationForm(
+            data={
+                "username": "peter7",
+                "email": "peter@matchapet.com",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 6)
+
+    def test_ShelterRegistrationForm_invalid_data7(self):
+        form = ShelterRegistrationForm(
+            data={
+                "email": "peter@matchapet.com",
             }
         )
 
@@ -67,6 +156,55 @@ class DefaultTestFroms(TestCase):
 
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 4)
+
+    def test_ShelterUpdateForm_invalid_data1(self):
+        form = ShelterUpdateForm(
+            data={
+                "username": "peter3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 5)
+
+    def test_ShelterUpdateForm_valid2(self):
+        form = ShelterUpdateForm(
+            data={
+                "username": "peter3",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 3)
+
+    def test_ShelterUpdateForm_valid3(self):
+        form = ShelterUpdateForm(
+            data={
+                "username": "peter3",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+                "shelter_city": "Brooklyn",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+
+    def test_ShelterUpdateForm_valid4(self):
+        form = ShelterUpdateForm(
+            data={
+                "username": "peter3",
+                "email": "peter@matchapet.com",
+                "first_name": "Peter",
+                "last_name": "Voltz",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 2)
 
 
 class TestForms(SimpleTestCase):
@@ -152,103 +290,3 @@ class TestForms(SimpleTestCase):
 
 
 # ******
-
-
-class BaseTest(TestCase):
-    def setUp(self):
-        self.register_url = reverse("accounts:register-shelter")
-        self.dummy_user = ShelterRegisterData.objects.create(
-            shelter_id=5,
-            username="peter7",
-            email="peter@matchapet.com",
-            first_name="Peter",
-            last_name="Voltz",
-            shelter_city="Manhattan",
-            shelter_state="New York",
-            password="test123abc",
-        )
-
-        self.user = {
-            "username": "peter7",
-            "email": "peter@matchapet.com",
-            "first_name": "Peter",
-            "last_name": "Voltz",
-            "shelter_city": "Manhattan",
-            "shelter_state": "New York",
-            "password1": "test123abc",
-            "password2": "test123abc",
-        }
-        self.user_no_username = {
-            "username": "",
-            "email": "peter@matchapet.com",
-            "first_name": "Peter",
-            "last_name": "Voltz",
-            "shelter_city": "Manhattan",
-            "shelter_state": "New York",
-            "password1": "test123abc",
-            "password2": "test123abc",
-        }
-
-        self.user_name_exists = {
-            "username": "peter7",
-            "email": "peter@matchapet.com",
-            "first_name": "Peter",
-            "last_name": "Voltz",
-            "shelter_city": "Manhattan",
-            "shelter_state": "New York",
-            "password1": "test123abc",
-            "password2": "test123abc",
-        }
-
-        self.user_email_exists = {
-            "username": "peter7",
-            "email": "peter@matchapet.com",
-            "first_name": "Peter",
-            "last_name": "Voltz",
-            "shelter_city": "Manhattan",
-            "shelter_state": "New York",
-            "password1": "test123abc",
-            "password2": "test123abc",
-        }
-
-        self.user_hm_invalid_email = {
-            "username": "peter7",
-            "email": "pet@matchapet.com",
-            "first_name": "Peter",
-            "last_name": "Voltz",
-            "shelter_city": "Manhattan",
-            "shelter_state": "New York",
-            "password1": "test123abc",
-            "password2": "test123abc",
-        }
-
-        return super().setUp
-
-
-class RegisterFormTest(BaseTest):
-    def test_form_not_valid(self):
-        form = ShelterRegistrationForm(self.user_no_username)
-        self.assertFalse(form.is_valid())
-
-    def test_form_valid(self):
-        form = ShelterRegistrationForm(self.user)
-        self.assertFalse(form.is_valid())
-
-    def test_form_username_exists(self):
-        form = ShelterRegistrationForm(self.user_name_exists)
-        form.has_error("username", "A user with that username already exists.")
-
-    def test_form_email_exists(self):
-        form = ShelterRegistrationForm(self.user_email_exists)
-        form.has_error(
-            "email",
-            "This Email is already registered. "
-            "Please use a different email address.",
-        )
-
-    def test_form_hm_invalid_email(self):
-        form = ShelterRegistrationForm(self.user_hm_invalid_email)
-        form.has_error(
-            "This email is not a valid Email Address for Hiring Manager. "
-            "Please use a different email address."
-        )
