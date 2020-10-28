16e7270af48c847236fa8bd8ac3a36399fb43c41
gururaj3 <guruz.shinde@gmail.com>
2020-10-27 23:24:48 -0400

    changing setings.py

--- match_a_pet/settings.py
+++ match_a_pet/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
 from pathlib import Path
 import os
+
+if "HEROKU" in os.environ:
+    import django_heroku
+
+    django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,7 +133,6 @@ MEDIA_URL = "/media/"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "../../"
 LOGIN_URL = "../../login/shelter"
-django_heroku.settings(locals())
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
