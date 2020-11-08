b692cc2fa88987dbac3ac9a973325eace7bddf9d
gururaj3 <guruz.shinde@gmail.com>
2020-11-08 01:49:47 -0500

    changing forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 # from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import ShelterRegisterData, Pet
+from .models import ShelterRegisterData, Pet, UserRegisterData, User
 
 # from django.forms import ModelForm
 # from django.db import models
@@ -24,21 +24,25 @@ class ShelterRegistrationForm(UserCreationForm):
         ("Staten Island", "Staten Island"),
         ("Bronx", "Bronx"),
     )
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    shelter_city = forms.ChoiceField(choices=ny_choices)
-    shelter_state = forms.ChoiceField(choices=l_choices)
+    # email = forms.EmailField(required=True)
+    # first_name = forms.CharField(required=True)
+    # last_name = forms.CharField(required=True)
+    address = forms.CharField(required=True)
+    city = forms.ChoiceField(choices=ny_choices)
+    state = forms.ChoiceField(choices=l_choices)
+    zip_code = forms.CharField(required=True)
 
     class Meta:
-        model = ShelterRegisterData
+        model = User
         fields = [
             "username",
             "email",
             "first_name",
             "last_name",
-            "shelter_city",
-            "shelter_state",
+            "address",
+            "city",
+            "state",
+            "zip_code",
             "password1",
             "password2",
         ]
@@ -47,7 +51,49 @@ class ShelterRegistrationForm(UserCreationForm):
         }
 
 
-class ShelterUpdateForm(forms.ModelForm):
+class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].label = "User Email"
+        self.fields["username"].label = "User Name"
+        self.fields["first_name"].label = "User Staff First Name"
+        self.fields["last_name"].label = "User Staff Last Name"
+
+    l_choices = (("New York", "New York"), ("California", "California"))
+    ny_choices = (
+        ("Manhattan", "Manhattan"),
+        ("Brooklyn", "Brooklyn"),
+        ("Queens", "Queens"),
+        ("Staten Island", "Staten Island"),
+        ("Bronx", "Bronx"),
+    )
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    city = forms.ChoiceField(choices=ny_choices)
+    state = forms.ChoiceField(choices=l_choices)
+    zip_code = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "address",
+            "city",
+            "state",
+            "zip_code",
+            "password1",
+            "password2",
+        ]
+        help_texts = {
+            "username": ("Shelter name can contain Letters, digits and @/./+/-/_ only.")
+        }
+
+
+class ShelterUserUpdateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["email"].label = "Shelter Email"
@@ -66,24 +112,79 @@ class ShelterUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
-    shelter_city = forms.ChoiceField(choices=ny_choices)
-    shelter_state = forms.ChoiceField(choices=l_choices)
+    city = forms.ChoiceField(choices=ny_choices)
+    state = forms.ChoiceField(choices=l_choices)
+    zip_code = forms.CharField(required=True)
 
     class Meta:
-        model = ShelterRegisterData
+        model = User
         fields = [
             "username",
             "email",
             "first_name",
             "last_name",
-            "shelter_city",
-            "shelter_state",
-            "shelter_profile_image",
+            "address",
+            "city",
+            "state",
+            "zip_code",
         ]
-        labels = {"shelter_profile_image": ("Shelter Profile Picture")}
         help_texts = {
             "username": ("Shelter name can contain Letters, digits and @/./+/-/_ only.")
         }
+
+
+class ShelterUpdateForm(forms.ModelForm):
+    class Meta:
+        model = ShelterRegisterData
+        fields = ["shelter_profile_image"]
+        labels = {"shelter_profile_image": ("Shelter Profile Picture")}
+
+
+class ClientUserUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].label = "Shelter Email"
+        self.fields["username"].label = "Shelter Name"
+        self.fields["first_name"].label = "Shelter Staff First Name"
+        self.fields["last_name"].label = "Shelter Staff Last Name"
+
+    l_choices = (("New York", "New York"), ("California", "California"))
+    ny_choices = (
+        ("Manhattan", "Manhattan"),
+        ("Brooklyn", "Brooklyn"),
+        ("Queens", "Queens"),
+        ("Staten Island", "Staten Island"),
+        ("Bronx", "Bronx"),
+    )
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    city = forms.ChoiceField(choices=ny_choices)
+    state = forms.ChoiceField(choices=l_choices)
+    zip_code = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "address",
+            "city",
+            "state",
+            "zip_code",
+        ]
+        help_texts = {
+            "username": ("Shelter name can contain Letters, digits and @/./+/-/_ only.")
+        }
+
+
+class ClientUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserRegisterData
+        fields = ["user_profile_image"]
+        labels = {"user_profile_image": ("User Profile Picture")}
 
 
 class PetForm(forms.ModelForm):
