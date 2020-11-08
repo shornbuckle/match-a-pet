917ad869b4bcbfcc6bae30b69e241526765c8633
gururaj3 <guruz.shinde@gmail.com>
2020-11-08 15:09:33 -0500

    fixing errors

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 from .views import PetListView
-
-# from django.contrib.auth import login
-# from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
