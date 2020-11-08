4f84b1728b98a3be9d7bc72909874036045b4e73
gururaj3 <guruz.shinde@gmail.com>
2020-11-08 14:05:06 -0500

    fixing forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,6 @@
 from django import forms
-
-# from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import ShelterRegisterData, Pet, UserRegisterData, User
-
-# from django.forms import ModelForm
-# from django.db import models
 
 
 class ShelterRegistrationForm(UserCreationForm):
@@ -24,9 +19,9 @@ class ShelterRegistrationForm(UserCreationForm):
         ("Staten Island", "Staten Island"),
         ("Bronx", "Bronx"),
     )
-    # email = forms.EmailField(required=True)
-    # first_name = forms.CharField(required=True)
-    # last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     address = forms.CharField(required=True)
     city = forms.ChoiceField(choices=ny_choices)
     state = forms.ChoiceField(choices=l_choices)
