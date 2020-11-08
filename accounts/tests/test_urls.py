eb334ae9dc24df6e7cd67048826551d7e78cb360
gururaj3 <guruz.shinde@gmail.com>
2020-11-08 14:49:50 -0500

    adding tests

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -1,6 +1,15 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from accounts.views import registerShelter, shelterProfile, petsRegister, PetListView
+from accounts.views import (
+    registerShelter,
+    shelterProfile,
+    registerUser,
+    petsRegister,
+    PetListView,
+    VerificationView,
+    petProfile,
+    shelter_profile,
+)
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -12,7 +21,7 @@ class TestUrls(SimpleTestCase):
 
     def test_shelter_register_url(self):
         url = reverse("accounts:register-user")
-        self.assertEquals(resolve(url).func, registerShelter)
+        self.assertEquals(resolve(url).func, registerUser)
         self.assertEquals(resolve(url).route, "user/register/")
 
     def test_shelter_login_url(self):
@@ -39,3 +48,18 @@ class TestUrls(SimpleTestCase):
         url = reverse("accounts:view-pets")
         self.assertEquals(resolve(url).func.view_class, PetListView)
         self.assertEquals(resolve(url).route, "pets/view_pets/")
+
+    def test_pets_profile_url(self):
+        url = reverse("accounts:pet-profile", args=["2"])
+        self.assertEquals(resolve(url).func, petProfile)
+        self.assertEquals(resolve(url).route, "pets/<id>/")
+
+    def test_shelter_profile_url(self):
+        url = reverse("accounts:shelterprofile", args=["peter7"])
+        self.assertEquals(resolve(url).func, shelter_profile)
+        self.assertEquals(resolve(url).route, "profile/<username>/")
+
+    def test_email_activate_url(self):
+        url = reverse("accounts:activate", args=["avhhk4ll2lbl2", "67172"])
+        self.assertEquals(resolve(url).func.view_class, VerificationView)
+        self.assertEquals(resolve(url).route, "activate/<uidb64>/<token>")
