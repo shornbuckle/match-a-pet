212177c8a7dfdc4d94cedede32c434c27d1e176a
Sean Hornbuckle <65291545+shornbuckle@users.noreply.github.com>
2020-11-17 23:19:13 -0500

    Delete urls.py

--- map/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    path("maps/shelters/", views.map_func, name="map-shelters"),
-    path("maps/test/", views.map_test, name="map-test"),
-    path("maps/test2/", views.map_test2, name="map-test"),
-]
