8454e07827c9a89aa34fe1c8f892286ec98e3215
gururaj3 <guruz.shinde@gmail.com>
2020-11-18 02:50:12 -0500

    testing

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -104,21 +104,22 @@ class TestViews(TestCase):
         self.assertEquals(str(self.dummy_user.state), "New York")
         self.assertEquals(str(self.dummy_user.zip_code), "11209")
 
+
 class TestUserRegisterView(TestCase):
     def test_view_register_page(self):
         client = Client()
-        
+
         self.dummy_user = User.objects.create(
-                    username="peter7",
-                    email="peter@matchapet.com",
-                    first_name="Peter",
-                    last_name="Voltz",
-                    address="5th Ave",
-                    city="Manhattan",
-                    state="New York",
-                    zip_code="11209",
-                    password="test123abc",
-                )
+            username="peter7",
+            email="peter@matchapet.com",
+            first_name="Peter",
+            last_name="Voltz",
+            address="5th Ave",
+            city="Manhattan",
+            state="New York",
+            zip_code="11209",
+            password="test123abc",
+        )
         self.url = reverse("accounts:shelter-profile")
         response = client.post(
             self.url,
@@ -137,10 +138,11 @@ class TestUserRegisterView(TestCase):
         )
         self.assertEqual(response.status_code, 302)
 
+
 # class TestUserRegisterView(TestCase):
 #     def test_view_register_page(self):
 #         client = Client()
-        
+
 #         self.dummy_user = User.objects.create(
 #                     username="peter7",
 #                     email="peter@matchapet.com",
@@ -185,121 +187,121 @@ class TestUserRegisterView(TestCase):
 #         )
 #         self.assertEqual(response.status_code, 200)
 
-    # def test_shelter_register_post_view1(self):
-    #     client = Client()
-    #     self.registerURL = reverse("accounts:register-shelter")
-    #     self.dummy_user = ShelterRegisterData.objects.create(
-    #         # shelter_id=5,
-    #         username="peter7",
-    #         email="peter@matchapet.com",
-    #         first_name="Peter",
-    #         last_name="Voltz",
-    #         shelter_city="Manhattan",
-    #         shelter_state="New York",
-    #         password="test123abc",
-    #     )
+# def test_shelter_register_post_view1(self):
+#     client = Client()
+#     self.registerURL = reverse("accounts:register-shelter")
+#     self.dummy_user = ShelterRegisterData.objects.create(
+#         # shelter_id=5,
+#         username="peter7",
+#         email="peter@matchapet.com",
+#         first_name="Peter",
+#         last_name="Voltz",
+#         shelter_city="Manhattan",
+#         shelter_state="New York",
+#         password="test123abc",
+#     )
 
-    #     response = client.post(
-    #         self.registerURL,
-    #         {
-    #             "username": "peter7",
-    #             "email": "peter@matchapet.com",
-    #             "first_name": "Peter",
-    #             "last_name": "Voltz",
-    #             "shelter_city": "Manhattan",
-    #             "shelter_state": "New York",
-    #             "password1": "test123abc",
-    #             "password2": "test123abc",
-    #         },
-    #     )
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "accounts/register.html")
-    #     self.assertEquals(str(self.dummy_user.email), "peter@matchapet.com")
-    #     self.assertEquals(str(self.dummy_user.username), "peter7")
-    #     self.assertEquals(str(self.dummy_user.first_name), "Peter")
-    #     self.assertEquals(str(self.dummy_user.last_name), "Voltz")
-    #     self.assertEquals(str(self.dummy_user.shelter_state), "New York")
+#     response = client.post(
+#         self.registerURL,
+#         {
+#             "username": "peter7",
+#             "email": "peter@matchapet.com",
+#             "first_name": "Peter",
+#             "last_name": "Voltz",
+#             "shelter_city": "Manhattan",
+#             "shelter_state": "New York",
+#             "password1": "test123abc",
+#             "password2": "test123abc",
+#         },
+#     )
+#     self.assertEquals(response.status_code, 200)
+#     self.assertTemplateUsed(response, "accounts/register.html")
+#     self.assertEquals(str(self.dummy_user.email), "peter@matchapet.com")
+#     self.assertEquals(str(self.dummy_user.username), "peter7")
+#     self.assertEquals(str(self.dummy_user.first_name), "Peter")
+#     self.assertEquals(str(self.dummy_user.last_name), "Voltz")
+#     self.assertEquals(str(self.dummy_user.shelter_state), "New York")
 
-    # def test_shelter_update_post_view1(self):
-    #     client = Client()
-    #     self.registerURL = reverse("accounts:shelter-profile")
-    #     self.dummy_user = ShelterRegisterData.objects.create(
-    #         # shelter_id=5,
-    #         username="peter7",
-    #         email="peter@matchapet.com",
-    #         first_name="Peter",
-    #         last_name="Voltz",
-    #         shelter_city="Manhattan",
-    #         shelter_state="New York",
-    #         password="test123abc",
-    #     )
+# def test_shelter_update_post_view1(self):
+#     client = Client()
+#     self.registerURL = reverse("accounts:shelter-profile")
+#     self.dummy_user = ShelterRegisterData.objects.create(
+#         # shelter_id=5,
+#         username="peter7",
+#         email="peter@matchapet.com",
+#         first_name="Peter",
+#         last_name="Voltz",
+#         shelter_city="Manhattan",
+#         shelter_state="New York",
+#         password="test123abc",
+#     )
 
-    #     shelterid = ShelterRegisterData.objects.get(email="peter@matchapet.com")
+#     shelterid = ShelterRegisterData.objects.get(email="peter@matchapet.com")
 
-    #     response = client.post(self.registerURL, {})
-    #     self.assertEquals(response.status_code, 302)
-    #     self.assertEquals(shelterid.username, "peter7")
-    #     self.assertEquals(shelterid.email, "peter@matchapet.com")
-    #     self.assertEquals(str(self.dummy_user.email), "peter@matchapet.com")
-    #     self.assertEquals(str(self.dummy_user.username), "peter7")
-    #     self.assertEquals(str(self.dummy_user.first_name), "Peter")
-    #     self.assertEquals(str(self.dummy_user.last_name), "Voltz")
-    #     self.assertEquals(str(self.dummy_user.shelter_state), "New York")
+#     response = client.post(self.registerURL, {})
+#     self.assertEquals(response.status_code, 302)
+#     self.assertEquals(shelterid.username, "peter7")
+#     self.assertEquals(shelterid.email, "peter@matchapet.com")
+#     self.assertEquals(str(self.dummy_user.email), "peter@matchapet.com")
+#     self.assertEquals(str(self.dummy_user.username), "peter7")
+#     self.assertEquals(str(self.dummy_user.first_name), "Peter")
+#     self.assertEquals(str(self.dummy_user.last_name), "Voltz")
+#     self.assertEquals(str(self.dummy_user.shelter_state), "New York")
 
-    # def test_pet_register_post_view(self):
-    #     client = Client()
-    #     self.dummy_user = ShelterRegisterData.objects.create(
-    #         email="peter@matchapet.com",
-    #     )
-    #     self.dummy_pet = Pet.objects.create(
-    #         id=self.dummy_user,
-    #         # pet_id="5",
-    #         pet_name="Sheila",
-    #         pet_breed="Dog",
-    #         pet_age="4",
-    #         pet_color="White",
-    #         pet_gender="Female",
-    #         # date_entered="12/1/2020",
-    #     )
+# def test_pet_register_post_view(self):
+#     client = Client()
+#     self.dummy_user = ShelterRegisterData.objects.create(
+#         email="peter@matchapet.com",
+#     )
+#     self.dummy_pet = Pet.objects.create(
+#         id=self.dummy_user,
+#         # pet_id="5",
+#         pet_name="Sheila",
+#         pet_breed="Dog",
+#         pet_age="4",
+#         pet_color="White",
+#         pet_gender="Female",
+#         # date_entered="12/1/2020",
+#     )
 
-    #     petid = Pet.objects.get(pet_name="Sheila")
-    #     # shelterid = ShelterRegisterData.objects.get(shelter_id=5)
+#     petid = Pet.objects.get(pet_name="Sheila")
+#     # shelterid = ShelterRegisterData.objects.get(shelter_id=5)
 
-    #     response = client.post(reverse("accounts:pet-register"), {})
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertEquals(str(petid.id), "peter@matchapet.com")
-    #     self.assertTemplateUsed(response, "accounts/pets.html")
-    #     self.assertEquals(str(self.dummy_pet.id), "peter@matchapet.com")
+#     response = client.post(reverse("accounts:pet-register"), {})
+#     self.assertEquals(response.status_code, 200)
+#     self.assertEquals(str(petid.id), "peter@matchapet.com")
+#     self.assertTemplateUsed(response, "accounts/pets.html")
+#     self.assertEquals(str(self.dummy_pet.id), "peter@matchapet.com")
 
-    # def test_pet_register_post_view1(self):
-    #     client = Client()
-    #     self.dummy_user = ShelterRegisterData.objects.create(
-    #         email="peter@matchapet.com",
-    #     )
-    #     self.dummy_pet = Pet.objects.create(
-    #         email=self.dummy_user,
-    #         pet_id="5",
-    #         pet_name="Sheila",
-    #         pet_breed="Dog",
-    #         pet_age="4",
-    #         pet_color="White",
-    #         pet_gender="Female",
-    #         date_entered="12/1/2020",
-    #     )
+# def test_pet_register_post_view1(self):
+#     client = Client()
+#     self.dummy_user = ShelterRegisterData.objects.create(
+#         email="peter@matchapet.com",
+#     )
+#     self.dummy_pet = Pet.objects.create(
+#         email=self.dummy_user,
+#         pet_id="5",
+#         pet_name="Sheila",
+#         pet_breed="Dog",
+#         pet_age="4",
+#         pet_color="White",
+#         pet_gender="Female",
+#         date_entered="12/1/2020",
+#     )
 
-    #     response = client.post(reverse("accounts:pet-register"), {
-    #         "email":"peter@matchapet.com",
-    #         "pet_name": "Sheila",
-    #         "pet_breed": "Dog",
-    #         "pet_age": "4",
-    #         "pet_color": "White",
-    #         "pet_gender": "Female",
-    #         "date_entered": "12/1/2020",
-    #     })
+#     response = client.post(reverse("accounts:pet-register"), {
+#         "email":"peter@matchapet.com",
+#         "pet_name": "Sheila",
+#         "pet_breed": "Dog",
+#         "pet_age": "4",
+#         "pet_color": "White",
+#         "pet_gender": "Female",
+#         "date_entered": "12/1/2020",
+#     })
 
-    #     petid = Pet.objects.get(pet_id=5)
+#     petid = Pet.objects.get(pet_id=5)
 
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "accounts/pets.html")
-    #     self.assertEquals(str(self.dummy_pet.email), "peter@matchapet.com")
-    #     self.assertEquals(self.dummy_pet.pet_name, "pet_name")
+#     self.assertEquals(response.status_code, 200)
+#     self.assertTemplateUsed(response, "accounts/pets.html")
+#     self.assertEquals(str(self.dummy_pet.email), "peter@matchapet.com")
+#     self.assertEquals(self.dummy_pet.pet_name, "pet_name")
