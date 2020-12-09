829d8f2a778a96069c62d0cb79ef8e22e64efd44
Benjamin Teo <benji7890@hotmail.com>
2020-12-10 04:40:58 +0800

    tests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -432,6 +432,11 @@ class TestProfile(TestCase):
         response = self.client.get(self.favorites_url)
         self.assertEqual(response.status_code, 302)
 
+    def test_adoption(self):
+        adoption_url = reverse("accounts:adopt_pending", args=["1"])
+        response = self.client.get(adoption_url)
+        self.assertEqual(response.status_code, 302)
+
     def test_favorite_list(self):
 
         self.client.login(username="peter8", password="test123abc")
